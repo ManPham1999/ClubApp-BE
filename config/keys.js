1b5817110ad32a602261ac04a3dab5edc91ca67e
ManPham1999 <manpham130699@gmail.com>
2021-03-11 17:12:22 +0700

    initial create

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI:
+		'mongodb+srv://ManPham1999:phamminhman2113@manpham1999.la6xx.mongodb.net/ClubManagementApp?retryWrites=true&w=majority',
+};
---
+++ routers/api/admin/index.js
@@ -0,0 +1,28 @@
+// 3rd packages
+const express = require('express');
+const router = express.Router();
+const adminController = require('./admin');
+
+// midleware
+const {authenticating, authorizing} = require('../../../middlewares/auth');
+
+// router: /api/admin/register
+//desc: register an admin
+// access: private
+router.post('/register', adminController.registerAdmin);
+
+// router: /api/admin/register
+//desc: register an admin
+// access: private
+router.post('/registerStaff', adminController.registerStaff);
+
+// router: /api/admin/
+//desc: login admin
+// access: private
+router.post('/login', adminController.loginAdmin);
+// router: /api/admin/removeImageOnCloud/:id
+//desc: login admin
+// access: private
+router.put('/removeImageOnCloud/:id', adminController.deletePhotoOnCloud);
+
+module.exports = router;
